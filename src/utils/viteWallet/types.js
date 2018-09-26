ca21f030dd7ef3fa7afd310a81800c4703c0b304
lovelycs <419579547@qq.com>
2018-09-27 00:55:36 +0800

    sent tx

--- src/utils/viteWallet/types.js
+++ src/utils/viteWallet/types.js
@@ -1,21 +1,6 @@
 class Types {
-    constructor(utils) {
-        this.utils = utils;
-    }
-
     isValidHexAddr(addr) {
         return $ViteJS.Vite.Types.isValidHexAddr(addr);
-    }
-    isValidMessage(message) {
-        if (/\s+/g.test(message)) {
-            return 0;
-        }
-        let utf8Bytes = this.utils.strToUtf8Bytes(message);
-        let hexStr = utf8Bytes ? this.utils.bytesToHex(utf8Bytes) : '';
-        if (hexStr.length > 180) {
-            return 1;
-        }
-        return -1;
     }
 }
 
