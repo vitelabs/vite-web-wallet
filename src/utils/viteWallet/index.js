f18487b408c37f1deae097cbf31146bea8720418
lovelycs <419579547@qq.com>
2018-09-27 13:35:51 +0800

    qrcode z-index

--- src/utils/viteWallet/index.js
+++ src/utils/viteWallet/index.js
@@ -7,7 +7,7 @@ import bignumber from './bignumber';
 import Types from './types';
 
 let WS_RPC = new ViteJS.WS_RPC({
-    url: 'ws://150.109.54.158:31420',
+    url: 'wss://test.vitewallet.com',
     timeout: 15000
 });
 window.$ViteJS = new ViteJS(WS_RPC);
