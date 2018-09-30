3a715eb4fc3c2c38485c9c1d9e83ed022f6677e5
lovelycs <419579547@qq.com>
2018-09-30 15:47:38 +0800

    test

--- src/index.js
+++ src/index.js
@@ -46,13 +46,13 @@ const router = new VueRouter({
 });
 
 router.beforeEach((to, from, next) => {
-    if (!from.name && to.name !== 'index') {
-        router.replace({
-            name: 'index'
-        });
-        return;
-    }
-
+    // if (!from.name && to.name !== 'index') {
+    //     router.replace({
+    //         name: 'index'
+    //     });
+    //     return;
+    // }
+    console.log('???????');
     _hmt.push(['_trackPageview', to.path]);
     next();
 });
