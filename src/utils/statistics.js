0ce7efa2a3c7a98fc36d2df7eaf8bbdda97037c9
lovelycs <419579547@qq.com>
2018-10-16 23:33:49 +0800

    statistics and version

---
+++ src/utils/statistics.js
@@ -0,0 +1,51 @@
+let statisticsNull = {
+    pageView(...args){
+        console.log('[statistics pageView]', JSON.stringify(args));
+    },
+    event(...args){
+        console.log('[statistics event]', JSON.stringify(args));
+    }
+};
+
+let statistics = {
+    pageView(path) {
+        _hmt.push(['_trackPageview', path]);
+    },
+    event(...args) {
+        if (args.length < 2) {
+            return;
+        }
+        _hmt.push(['_trackEvent'].concat(args));
+    }
+};
+
+window.onload = function () {
+    if (!window.performance) {
+        return;
+    }
+    try {
+        var fetchStart = window.performance.timing.fetchStart;  // any requests
+
+        // DNS
+        var DNSStart = window.performance.timing.domainLookupStart; // dns end
+        var DNSEnd = window.performance.timing.domainLookupEnd; // dns end
+        var DNSTimes = DNSEnd - DNSStart;
+        console.log('DNSTimes', DNSTimes);
+        _hmt.push(['_trackEvent', 'performance', 'DNS', 'time', DNSTimes]);
+
+        // load
+        var firstByteTime = window.performance.timing.responseStart;
+
+        var whiteTimes = firstByteTime - fetchStart;
+        console.log('whiteTimes', whiteTimes);
+        _hmt.push(['_trackEvent', 'performance', 'white', 'time', whiteTimes]);
+
+        var loadedTimes = new Date().getTime() - fetchStart;
+        console.log('loadedTimes', loadedTimes);
+        _hmt.push(['_trackEvent', 'performance', 'loaded', 'time', loadedTimes]);
+    } catch(err) {
+        console.warn(err);
+    }
+};
+
+export default process.env.NODE_ENV === 'production' ? statistics : statisticsNull;
