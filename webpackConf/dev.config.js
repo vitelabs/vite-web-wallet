9ca69908ef334b4cfe4341847527a7da2e0dc326
lovelycs <419579547@qq.com>
2018-10-18 17:37:01 +0800

    test token list

--- webpackConf/dev.config.js
+++ webpackConf/dev.config.js
@@ -2,6 +2,13 @@ module.exports = {
     devServer: {
         quiet: false,
         host: '127.0.0.1',
-        port: 8081
+        port: 8081,
+        proxy: {
+            '/api': {
+                target: 'https://testnet.vite.net',
+                changeOrigin:true,
+                secure: false
+            }
+        }
     }
 };
