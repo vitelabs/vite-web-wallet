7cd7cb8c746fc5be8cf9fca308fb9b592d9e2c82
lovelycs <419579547@qq.com>
2018-10-23 14:45:44 +0800

    request

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,18 +1,18 @@
 import qs from 'qs';
 
-export default  function request({ method = 'GET', url, params = {} }) {
+export default  function request({ method = 'GET', path, params = {} }) {
     method = method.toUpperCase();
 
     const xhr = new XMLHttpRequest();
     const qsStr = qs.stringify(params);
 
     method === 'GET' && (
-        url.indexOf('?') < 0 ? 
-            (url = `${url}?${qsStr}`) : 
-            (url = `${url}${qsStr}`)
+        path.indexOf('?') < 0 ? 
+            (path = `${path}?${qsStr}`) : 
+            (path = `${path}${qsStr}`)
     );
 
-    xhr.open(method, url, true);
+    xhr.open(method, path, true);
     xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded; charset=utf-8');
     if (method === 'POST') {
         xhr.send(qsStr);
@@ -24,7 +24,16 @@ export default  function request({ method = 'GET', url, params = {} }) {
         xhr.onload = function () {
             if (xhr.status == 200) {
                 try {
-                    res(JSON.parse(xhr.responseText));
+                    let { code, msg, data, error } = JSON.parse(xhr.responseText);
+                    if (code !== 0) {
+                        return rej({
+                            code,
+                            message: msg || error
+                        });
+                    }
+
+                    data = data || null;
+                    res(data);
                 } catch (e) {
                     rej(e);
                 }
