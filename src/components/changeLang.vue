9e75e609326721f44c123ce3ad507a2d54193f39
lovelycs <419579547@qq.com>
2018-10-24 21:46:35 +0800

    locale

--- src/components/changeLang.vue
+++ src/components/changeLang.vue
@@ -24,6 +24,7 @@ export default {
         },
         changeLocale(locale) {
             this.$i18n.locale = locale;
+            window.viteWalletI18n && window.viteWalletI18n.setLocale(locale);
             localStorage.setItem('lang', locale);
             this.toggleLangList();
         }
