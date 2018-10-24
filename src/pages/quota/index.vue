a1cd13140876cedd40cbe45ab7955b1c5617df36
lovelycs <419579547@qq.com>
2018-10-24 20:53:35 +0800

    quota

--- src/pages/quota/index.vue
+++ src/pages/quota/index.vue
@@ -1,9 +1,22 @@
 <template>
-    <div></div>
+    <div class="quota-wrapper">
+        <quota-head></quota-head>
+        <div class="content">
+            <my-quota></my-quota>
+            <pledge-tx></pledge-tx>
+        </div>
+    </div>
 </template>
 
 <script>
+import quotaHead from './quotaHead';
+import myQuota from './myQuota';
+import pledgeTx from './pledgeTx';
+
 export default {
+    components: {
+        quotaHead, myQuota, pledgeTx
+    },
     mounted() {
         console.log('?????????/');
     }
