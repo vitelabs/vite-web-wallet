83aeca0ec851b4da43c87fc7c55098b3ee5f2697
lovelycs <419579547@qq.com>
2018-10-31 23:41:17 +0800

    test websocket

--- src/utils/viteWallet/index.js
+++ src/utils/viteWallet/index.js
@@ -10,8 +10,17 @@ import pledge from './pledge';
 process.env.NODE_ENV !== 'production' && console.log(process.env.goViteServer);
 
 let WS_RPC = new ViteJS.WS_RPC({
-    url: process.env.goViteServer,
+    // url: process.env.goViteServer,
     timeout: 60000
+});
+WS_RPC.on('connect', (m) => {
+    console.log('connect', m);
+});
+WS_RPC.on('error', (m) => {
+    console.log('error', m);
+});
+WS_RPC.on('close', (m) => {
+    console.log('close', m);
 });
 window.$ViteJS = new ViteJS(WS_RPC);
 
