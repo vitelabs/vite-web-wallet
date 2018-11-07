c51e44f48039616fc506036c58ce651afff0fcad
lovelycs <419579547@qq.com>
2018-11-07 16:46:49 +0800

    some

--- src/components/tooltips.vue
+++ src/components/tooltips.vue
@@ -6,37 +6,37 @@
 </template>
 <script>
 export default {
-  props: {
-    content: {
-      type: String,
-      default: ""
+    props: {
+        content: {
+            type: String,
+            default: ''
+        }
     }
-  }
 };
 </script>
 <style lang="scss" scoped>
 .tooltips {
-  position: absolute;
-  display: flex;
-  align-content: center;
-  left: 100%;
-  top:50%;
-  transform: translateY(-50%);
-  .trigle {
-    border-top: 7px solid transparent;
-    border-bottom: 7px solid transparent;
-    border-right: 7px solid #fff;
-    height: 0;
-    width:0;
-  }
-  .content {
-    background: #ffffff;
-    box-shadow: 0 5px 20px 0 rgba(0, 0, 0, 0.1);
-    border-radius: 2px;
-    width: 314px;
-    height: 100px;
-    padding: 10px;
-    box-sizing: border-box;
-  }
+    position: absolute;
+    display: flex;
+    align-content: center;
+    left: 100%;
+    top: 50%;
+    transform: translateY(-50%);
+    .trigle {
+        border-top: 7px solid transparent;
+        border-bottom: 7px solid transparent;
+        border-right: 7px solid #fff;
+        height: 0;
+        width:0;
+    }
+    .content {
+        background: #ffffff;
+        box-shadow: 0 5px 20px 0 rgba(0, 0, 0, 0.1);
+        border-radius: 2px;
+        text-align: center;
+        height: 100px;
+        padding: 10px;
+        box-sizing: border-box;
+    }
 }
 </style>
