8fd576ff02ca712a2e78cb749cb7b24c417b4914
lovelycs <419579547@qq.com>
2018-11-21 21:01:09 +0800

    test

---
+++ start-image.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BASE_PATH=$(pwd)
+REPO_PATH=$(cd $1 && pwd)
+
+docker run --rm -t -i \
+	-e NODE_VERSION \
+	-e NPM_VERSION \
+	-e RUBY_VERSION \
+	-e YARN_VERSION \
+	-e HUGO_VERSION \
+	-e PHP_VERSION \
+	-e GO_VERSION \
+	-v ${REPO_PATH}:/opt/repo \
+	-v ${BASE_PATH}/run-build.sh:/usr/local/bin/build \
+	-v ${BASE_PATH}/run-build-functions.sh:/usr/local/bin/run-build-functions.sh \
+	netlify/build /bin/bash
