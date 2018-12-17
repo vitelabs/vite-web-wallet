97cae7afa2c2118ee4522544384923bc55ada232
lovelycs <419579547@qq.com>
2018-12-17 10:45:16 +0800

    wsprovider

--- src/utils/viteWallet/index.js
+++ src/utils/viteWallet/index.js
@@ -1,17 +1,16 @@
 import { client } from '@vite/vitejs';
-import provider from '@vite/vitejs/dist/es5/provider/WS.js';
+import provider from 'WSprovider';
 
 import net from './net';
 import ledger from './ledger';
 
 // timout: 6000; retryTimes: 10; retryInterval: 10000
 let WS_RPC = new provider(process.env.goViteServer);
-WS_RPC.on('connect', () => {
-    viteWallet && viteWallet.Ledger.loopHeight();
+window.$ViteJS = new client(WS_RPC, () => {
     viteWallet && viteWallet.Ledger.getDefaultTokenList();
+    viteWallet && viteWallet.Ledger.loopHeight();
 });
 
-window.$ViteJS = new client(WS_RPC);
 window.viteWallet = {
     Net: new net(),
     Ledger: new ledger()
