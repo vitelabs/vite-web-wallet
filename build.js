8a1bf494d2516a94f4bc3b926d8ef3b25282a941
lovelycs <419579547@qq.com>
2018-12-25 14:42:26 +0800

    some

--- build.js
+++ build.js
@@ -1,12 +1,13 @@
 var fs = require('fs');
 var path = require('path');
 
-console.log(`Write _redirects ${process.env.NODE_ENV}`);
+console.log(`Write netlifyConf ${process.env.NODE_ENV}`);
 
 var redirect = process.env.NODE_ENV === 'test' ? 
-    path.join(__dirname, 'redirects/_redirects_test') :
-    path.join(__dirname, 'redirects/_redirects');
+    path.join(__dirname, 'netlifyConf/_redirects_test') :
+    path.join(__dirname, 'netlifyConf/_redirects');
 var staticPath = path.join(__dirname, 'static');
+var _headers = path.join(__dirname, 'netlifyConf/_headers');
 
 var result = fs.existsSync(staticPath);
 // Not exists
@@ -16,3 +17,4 @@ if (!result) {
 }
 
 fs.writeFileSync(path.join(staticPath, '_redirects'), fs.readFileSync(redirect));
+fs.writeFileSync(path.join(staticPath, '_headers'), fs.readFileSync(_headers));
