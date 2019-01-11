021251815a0644e86a3355846e9ee645a15c0297
lovelycs <419579547@qq.com>
2019-01-11 18:13:47 +0800

    bugfix hd

--- src/utils/ethWallet/address.js
+++ src/utils/ethWallet/address.js
@@ -2,19 +2,35 @@ const bip39 = require('bip39');
 const hdkey = require('ethereumjs-wallet/hdkey');
 const hdPathString = 'm/44\'/60\'/0\'/0';
 
-export default function(mnemonic, index) {
-    let root = getRoot(mnemonic);
+export function getWalletAddr(mnemonic, index) {
+    let root = getRoot(mnemonic);    
     let child = root.deriveChild(index);
     let wallet = child.getWallet();
+
     return {
         wallet,
         hexAddr: '0x' + wallet.getAddress().toString('hex')
     };
 }
 
+export function getWrongWalletAddr(mnemonic, index) {
+    let rootHex = getRootByHex(mnemonic);
+    let childHex = rootHex.deriveChild(index);
+    let walletHex = childHex.getWallet();
+    return {
+        wallet: walletHex,
+        hexAddr: '0x' + walletHex.getAddress().toString('hex')
+    };
+}
+
+function getRootByHex(mnemonic) {
+    let seed = bip39.mnemonicToSeedHex(mnemonic);
+    let hdWallet = hdkey.fromMasterSeed(seed);
+    return hdWallet.derivePath(hdPathString);
+}
 
 function getRoot(mnemonic) {
-    let seed = bip39.mnemonicToSeedHex(mnemonic);
+    let seed = bip39.mnemonicToSeed(mnemonic);
     let hdWallet = hdkey.fromMasterSeed(seed);
     return hdWallet.derivePath(hdPathString);
 }
