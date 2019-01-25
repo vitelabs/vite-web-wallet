c6ca007a9ff02a01156eb0665bb2282f20faf470
lovelycs <419579547@qq.com>
2019-01-25 11:54:06 +0800

    some

--- src/store/exchange/index.js
+++ src/store/exchange/index.js
@@ -2,10 +2,12 @@ import rate from './rate.js';
 import depth from './depth.js';
 import activeTxPair from './activeTxPair';
 import latestTx from './latestTx';
+import balance from './balance';
 
 export default {
     exchangeRate: rate,
     exchangeDepth: depth,
     exchangeActiveTxPair: activeTxPair,
-    exchangeLatestTx: latestTx
+    exchangeLatestTx: latestTx,
+    exchangeBalance: balance
 };
