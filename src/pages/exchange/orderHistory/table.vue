2cffc931fb2ac348abf7e4a3e2a69eaa50c0cc1b
hurrytospring <ddyz1993@163.com>
2019-02-12 18:01:19 +0800

    f

--- src/pages/exchange/orderHistory/table.vue
+++ src/pages/exchange/orderHistory/table.vue
@@ -1,440 +1,87 @@
 <template>
-  <div class="ex_tb">
-    <div class="head-row">
-      <div
-        v-for="(h,i) in $t('exchangeOrderHistory.table.heads')"
-        :key="h"
-      >{{h.replace("#tokenSymbol#","vite")}} <div
-          class="sort-btn"
-          @click="sortBy(i)"
-        ></div>
-      </div>
+    <div class="ex_tb">
+        <div class="head-row">
+            <div
+                v-for="(h,i) in $t('exchangeOrderHistory.table.heads')"
+                :key="h"
+            >{{h.replace("#tokenSymbol#","vite")}} <div
+                    class="sort-btn"
+                    @click="sortBy(i)"
+                ></div>
+            </div>
 
+        </div>
+        <div class="row-container">
+            <div
+                class="row"
+                v-for="v in sortedList"
+                :key="v.orderId"
+            >
+                <div>{{v.date}}</div>
+                <div>{{`${v.ftokenShow}/${v.ttokenShow}`}}</div>
+                <div :class="{buy:v.side===0,sell:v.side===1}">{{$t("exchangeOrderHistory.side")[v.side]}}</div>
+                <div>{{v.price}}</div>
+                <div>{{v.amount}}</div>
+                <div>{{v.filledQ}}</div>
+                <div>{{v.rate}}</div>
+                <div>{{v.average}}</div>
+                <div>{{v.fee}}</div>
+                <div>{{v.status}}</div>
+            </div>
+        </div>
     </div>
-    <div class="row-container">
-    <div
-      class="row"
-      v-for="l in sortedList"
-      :key="l[0]"
-    >
-      <div
-        v-for="c in l"
-        :key="c"
-      >{{c}}</div>
-    </div>
-    </div>
-  </div>
 </template>
 <script>
 export default {
-  props: {
-    list: {
-      type: Array,
-      default: () => [
-        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ],        [
-          Date.now(),
-          "adsfsad",
-          "adfds",
-          "lkklklklk",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc",
-          "ikilljn",
-          "xzvgfbd",
-          "dsafc"
-        ]
-      ]
+    props: {
+        list: {
+            type: Array,
+            default: () => []
+        }
+    },
+    data() {
+        return {
+            sortIndex: 0,
+            sortType: 1
+        };
+    },
+    methods: {
+        sortBy(i) {
+            if (i === this.sortIndex) {
+                this.sortType *= -1;
+                return;
+            }
+            this.sortIndex = i;
+        }
+    },
+    computed: {
+        sortedList() {
+            return this.list.sort((a, b) => {
+                return this.sortType * (a[this.sortIndex] - b[this.sortIndex]);
+            });
+        }
     }
-  },
-  data() {
-    return {
-      sortIndex: 0,
-      sortType: 1
-    };
-  },
-  methods: {
-    sortBy(i) {
-      if (i === this.sortIndex) {
-        this.sortType *= -1;
-        return;
-      }
-      this.sortIndex = i;
-    }
-  },
-  computed: {
-    sortedList() {
-      return this.list.sort((a, b) => {
-        return this.sortType * (a[this.sortIndex] - b[this.sortIndex]);
-      });
-    }
-  }
 };
 </script>
 <style lang="scss" scoped>
 @import "../components/table.scss";
-.ex_tb{
+.ex_tb {
     height: 100%;
     padding-bottom: 10px;
 }
 @include rowWith {
-  width: 8%;
-  &:first-child,
-  &:nth-child(4),
-  &:nth-child(5),
-  &:nth-child(6) {
-    width: 15%;
-  }
+    width: 8%;
+    &:first-child,
+    &:nth-child(4),
+    &:nth-child(5),
+    &:nth-child(6) {
+        width: 15%;
+    }
+}
+.buy {
+    color: #ff0008;
+}
+.sell {
+    color: #5bc500;
 }
 </style>
