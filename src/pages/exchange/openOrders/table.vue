94b478cd83f46ae563f2a90af3b6b940729c2435
hurrytospring <ddyz1993@163.com>
2019-02-20 09:30:30 +0800

    fix

--- src/pages/exchange/openOrders/table.vue
+++ src/pages/exchange/openOrders/table.vue
@@ -24,35 +24,96 @@
                 <div>{{v.rate}}</div>
                 <div>{{v.average}}</div>
                 <div
-                    @click="cancel()"
+                    @click="cancel(v)"
                     class="click-able"
                 >{{$t("exchangeOpenOrders.table.rowMap.cancel")}}</div>
             </div>
         </div>
+         <powProcess ref="pow"></powProcess>
     </div>
 </template>
 <script>
-import { order } from 'services/exchange';
+import { order, cancelOrder } from 'services/exchange';
+import powProcess from 'components/powProcess';
+const VoteDifficulty = '201564160';
 export default {
     data() {
         return {
-            list:[],
+            list: [],
             sortIndex: 0,
             sortType: 1,
-            acc:null,
-            addr:''
+            acc: null,
+            addr: ''
         };
     },
+    components:{powProcess},
     methods: {
-        cancel() {}
+        cancel(order) {
+            const failSubmit = e => {
+                const code =
+                    e && e.error ? e.error.code || -1 : e ? e.code : -1;
+                if (code === -35002) {
+                    let startTime = new Date().getTime();
+                    const powTxt = Object.assign(
+                        {},
+                        this.$t('quotaConfirmPoW')
+                    );
+                    powTxt.leftBtn.click = () => {
+                        this.$router.push({
+                            name: 'walletQuota'
+                        });
+                    };
+                    (powTxt.rightBtn.click = () => {
+                        this.$refs.pow
+                            .startPowTx(
+                                e.accountBlock,
+                                startTime,
+                                VoteDifficulty
+                            )
+                            .then(successSubmit)
+                            .catch(failSubmit);
+                    }),
+                    (powTxt.closeBtn = { show: true });
+                    this.$confirm(powTxt);
+                } else {
+                    this.$toast(
+                        this.$t(`exchangeOpenOrders.confirm.failToast`),e
+                    );
+                }
+            };
+            const successSubmit = () => {
+                this.$toast(
+                    this.$t(`exchangeOpenOrders.confirm.successToast`)
+                );
+            };
+            this.acc.initPwd(
+                {
+                    submitTxt: this.$t(`exchangeOpenOrders.confirm.submitTxt`),
+                    cancelTxt: this.$t('exchangeOpenOrders.confirm.cancelTxt'),
+                    submit: () => {
+                        cancelOrder({
+                            orderId: order.orderId,
+                            tradeToken: order.ftoken,
+                            side: order.side,
+                            quoteToken: order.ttoken
+                        })
+                            .then(successSubmit)
+                            .catch(e => {
+                                failSubmit(e);
+                            });
+                    }
+                },
+                true
+            );
+        }
     },
     beforeMount() {
-        this.acc=this.$wallet.getActiveAccount();
-        if(!this.acc)return;
-        this.acc&&(this.addr=this.acc.getDefaultAddr());
+        this.acc = this.$wallet.getActiveAccount();
+        if (!this.acc) return;
+        this.acc && (this.addr = this.acc.getDefaultAddr());
         order({
             address: this.addr,
-            status: 2
+            status: 1
         }).then(data => {
             this.list = data.orders;
         });
