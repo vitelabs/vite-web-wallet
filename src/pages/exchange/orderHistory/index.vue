a018af8c28f333b3b073a86a3cce5a949642ae6c
hurrytospring <ddyz1993@163.com>
2019-02-22 12:05:27 +0800

    f

--- src/pages/exchange/orderHistory/index.vue
+++ src/pages/exchange/orderHistory/index.vue
@@ -15,6 +15,7 @@ import Filters from './filters';
 import Table from './table';
 import { order } from 'services/exchange';
 import Pagination from 'components/pagination';
+import { timer } from "utils/asyncFlow";
 const pageSize=10;
 export default {
     components: {
@@ -33,11 +34,17 @@ export default {
             data: [],
             currentPage:1,
             totalPage:0,
-            filters:{}
+            filters:{},
+            timer:null
         };
     },
     beforeMount() {
+        this.timer=new timer(()=>this.update(),5000);
+        this.timer.start();
         this.update();
+    },
+    beforeDestroy(){
+        this.timer.stop();
     },
     methods: {
         toPage(pageNo){
