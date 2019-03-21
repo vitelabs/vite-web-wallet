903de55260161f04da5bf195d58e60f99d7cd241
hurrytospring <ddyz1993@163.com>
2019-03-21 20:21:32 +0800

    format

--- src/components/dialog/base.vue
+++ src/components/dialog/base.vue
@@ -24,7 +24,7 @@
 </template>
 
 <script>
-import mixin from './mixin'
+import mixin from './mixin';
 export default {
     mixins:[mixin]
 };
--- src/components/dialog/password.vue
+++ src/components/dialog/password.vue
@@ -9,6 +9,6 @@ export default {
     methods:{
 
     }
-}
+};
 </script>
 
