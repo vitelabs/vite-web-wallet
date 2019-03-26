3e9bf7266e5ab7871395020cfd596ba781d2ec2d
lovelycs <419579547@qq.com>
2019-03-26 15:39:25 +0800

    quota confirm and bugfix

---
+++ src/components/quota/pow.vue
@@ -0,0 +1,42 @@
+<template>
+    <confirm :title="$t('quotaConfirmPoW.title')" :showMask="showMask"
+             :leftBtnTxt="$t('quotaConfirmPoW.leftBtn.text')"
+             :leftBtnClick="_leftBtnClick"
+             :rightBtnTxt="$t('quotaConfirmPoW.rightBtn.text')"
+             :rightBtnClick="rightBtnClick"
+             :closeIcon="true" :close="closeBtnClick">
+        {{ $t('quotaConfirmPoW.content') }}
+    </confirm>
+</template>
+
+<script>
+import confirm from 'components/confirm';
+
+export default {
+    components: { confirm },
+    props: {
+        showMask: {
+            type: Boolean,
+            default: true
+        },
+        closeBtnClick: {
+            type: Function,
+            default: () => {}
+        },
+        rightBtnClick: {
+            type: Function,
+            default: () => {}
+        },
+        leftBtnClick: {
+            type: Function,
+            default: () => {}
+        }
+    },
+    methods: {
+        _leftBtnClick() {
+            this.leftBtnClick && this.leftBtnClick();
+            this.$router.push({ name: 'walletQuota' });
+        }
+    }
+};
+</script>
