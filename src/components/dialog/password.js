ae10f4f39647e41dc6e9a2eec2cff4640cb99b36
hurrytospring <ddyz1993@163.com>
2019-04-09 20:49:38 +0800

    f

---
+++ src/components/dialog/password.js
@@ -0,0 +1,4 @@
+import { getDialog } from './base.js';
+import password from './password.vue';
+
+export default getDialog(password);
---
+++ src/components/dialog/password.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        fdsaffaf
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return { inspector: [this.submit] };
+    },
+    methods: {
+        submit() {
+            return Promise.resolve({});
+        }
+    }
+};
+</script>
+
