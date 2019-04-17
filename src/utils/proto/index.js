42624c6d3030c91eacf684e1b2c28edb94d26c4d
lovelycs <419579547@qq.com>
2019-04-17 20:06:24 +0800

    websocket log

--- src/utils/proto/index.js
+++ src/utils/proto/index.js
@@ -8,6 +8,8 @@ class WsProtoClient {
     constructor(wsUrl) {
         this.MESSAGETYPE = { SUB: 'sub', UNSUB: 'un_sub', PING: 'ping', PONG: 'pong', PUSH: 'push' };
         this._clientId = random(10);
+        console.log('new Wesocket', this._clientId, new Date());
+
         this._subKeys = {};
 
         this._heartBeat = new timer(() => {
@@ -19,6 +21,13 @@ class WsProtoClient {
         try {
             const connect = new WebSocket(wsUrl);
             this.connect = connect;
+
+            window.onbeforeunload = function () {
+                console.log('关闭WebSocket连接！');
+                this.connect.onclose = function () {};
+                this.connect.close();
+            };
+
             connect.binaryType = 'arraybuffer';
 
             this._subKey && this.subscribe({ event_key: this._subKey });
@@ -31,11 +40,15 @@ class WsProtoClient {
                 // See ConversionOptions
                 const data = proto.toObject(message, { /* Longs: String, enums: String, bytes: String*/ });
 
+                if (data.client_id !== this._clientId) {
+                    console.log('clientId不一致', data.client_id, this._clientId);
+                    return;
+                }
                 if (data.op_type !== this.MESSAGETYPE.PUSH) return;
 
                 const realData = data.error_code ? null : JSON.parse(data.message);
 
-                console.log('onmessage', data.event_key, realData);
+                console.log('onmessage', data, realData);
 
                 const error = data.error_code || undefined;
                 this._subKeys[data.event_key] && this._subKeys[data.event_key].forEach(c => {
@@ -76,6 +89,10 @@ class WsProtoClient {
     send(event_key = '', type = this.MESSAGETYPE.PING) {
         if (!this.ready) return;
 
+        if (type === this.MESSAGETYPE.PING) {
+            console.log('ping', this._clientId, new Date());
+        }
+
         const payload = {
             event_key: event_key,
             op_type: type,
@@ -94,3 +111,6 @@ class WsProtoClient {
 }
 
 export const client = new WsProtoClient(process.env.pushServer);
+
+// const _client = new WsProtoClient(process.env.pushServer);
+// console.log(_client);
