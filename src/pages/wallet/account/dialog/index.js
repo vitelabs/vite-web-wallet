f277e7c7a8de52e1186048370e749353ed544eb1
hurrytospring <ddyz1993@163.com>
2019-04-22 00:50:15 +0800

    fix

--- src/pages/wallet/account/dialog/index.js
+++ src/pages/wallet/account/dialog/index.js
@@ -3,8 +3,10 @@ import receiveComp from './receive.vue';
 import chargeComp from './charge.vue';
 import addTokenComp from './addToken.vue';
 import tokenInfoComp from './tokenInfo.vue';
+import withdrawComp from './withdraw.vue';
 
 export const receiveDialog = getDialog(receiveComp, { showBottom: false });
 export const chargeDialog = getDialog(chargeComp, { showBottom: false });
 export const addTokenDialog = getDialog(addTokenComp);
 export const tokenInfoDialog = getDialog(tokenInfoComp);
+export const withdrawDialog = getDialog(withdrawComp);
