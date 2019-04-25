fa4d129acdd98cd6e45aa32651c58239419fe87a
hurrytospring <ddyz1993@163.com>
2019-04-25 17:22:51 +0800

    format

--- src/pages/wallet/account/dialog/charge.vue
+++ src/pages/wallet/account/dialog/charge.vue
@@ -58,7 +58,7 @@ export default {
             return bigNumber.toBasic(this.minimumDepositAmountMin, this.token.decimals);
         },
         addressQrcode() {
-            if(this.token.type==='OFFICAL_GATE'&&this.token.tokenSymbol==='BTC')return `bitcoin:${this.address}`
+            if (this.token.type === 'OFFICAL_GATE' && this.token.tokenSymbol === 'BTC') return `bitcoin:${ this.address }`;
             return this.address;
         }
     }
