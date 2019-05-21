3f355de1e15cf9085b3f4f5d081652da866f4155
lovelycs <419579547@qq.com>
2019-05-21 15:54:08 +0800

    update vitejs

--- webpackConf/plugins.js
+++ webpackConf/plugins.js
@@ -12,7 +12,7 @@ const Buffer_Path = path.join(__dirname, '../node_modules/buffer/index.js');
 const goViteServer = {
     production: '\'wss://api.vitewallet.com/ws\'',
     test: '\'wss://premainnet.vitewallet.com/test/ws\'',
-    dev: '\'wss://premainnet.vitewallet.com/test/ws\'',
+    dev: '\'ws://140.143.8.170:41423\'',
     dexTestNet: '\'wss://testnet.vitewallet.com/beta/ws\''
 };
 const viteNet = {
