e94003332a41f6e1721b5a5ebdec1dcc6f42e83f
lovelycs <419579547@qq.com>
2019-05-22 01:23:20 +0800

    some

--- src/pages/keystore/index.vue
+++ src/pages/keystore/index.vue
@@ -14,12 +14,18 @@
                   :unlockSuccess="unlockSuccess"></lock>
 
             <div v-show="keystore && account" class="unlock-wrapper">
-                <div class="unlock-btn __btn __btn_all_in __pointer">
-                    GetBalance
+                <br/>
+                <div v-if="balance">
+                    <div>{{ JSON.stringify(balance.balance) }}</div>
+                    <div>{{ JSON.stringify(balance.onroad) }}</div>
                 </div>
-                <div class="unlock-btn __btn __btn_all_in __pointer">
-                    AutoReceiveTx
+                <div v-show="!isActive && balance" class="unlock-btn __btn __btn_all_in __pointer" @click="activate">
+                    Auto Receive Tx
                 </div>
+                <div class="bottom __btn __btn_input">
+                    <input v-model="toAddress" />
+                </div>
+                <div @click="sendAllBalance" class="unlock-btn __btn __btn_all_in __pointer">Send Tx</div>
             </div>
         </div>
     </layout>
@@ -29,15 +35,25 @@
 import lock from './lock';
 import importKeystore from './import';
 import layout from '../start/layout';
+import { timer } from 'utils/asyncFlow';
+
+let balanceTimer = null;
 
 export default {
     components: { layout, importKeystore, lock },
+    destroyed() {
+        this.stopLoopBalance();
+        this.account.freeze();
+    },
     data() {
         return {
             address: '',
             name: '',
             keystore: null,
-            account: null
+            account: null,
+            balance: null,
+            isActive: false,
+            toAddress: ''
         };
     },
     methods: {
@@ -47,11 +63,61 @@ export default {
             this.name = obj.name || '';
             console.log(obj, type);
         },
+        activate() {
+            this.account.activate(2000, true, true);
+            this.isActive = true;
+        },
         unlockSuccess(account) {
             this.account = account;
-            this.account.activate(2000, true, true);
+            this.startLoopBalance();
+        },
+        startLoopBalance() {
+            this.stopLoopBalance();
 
-            console.log(this.account.balance);
+            balanceTimer = new timer(() => {
+                if (!this.isActive) {
+                    this.balance = this.account.balance;
+                    return;
+                }
+
+                return this.account.getBalance().then(data => {
+                    this.balance = data;
+                });
+            }, 2000);
+
+            balanceTimer.start();
+        },
+        stopLoopBalance() {
+            balanceTimer && balanceTimer.stop();
+            balanceTimer = null;
+        },
+        sendAllBalance() {
+            if (!this.balance || !this.balance.balance) {
+                this.$toast('No Balance!');
+                return;
+            }
+
+            const reqList = [];
+            const balanceInfos = this.balance.balance.tokenBalanceInfoMap ? this.balance.balance.tokenBalanceInfoMap : {};
+            for (const tokenId in balanceInfos) {
+                const item = balanceInfos[tokenId];
+
+                const amount = item.totalAmount;
+                if (+amount === 0) {
+                    continue;
+                }
+                reqList.push(this.account.sendTx({
+                    toAddress: this.toAddress,
+                    amount,
+                    tokenId
+                }, true, true));
+            }
+
+            Promise.all(reqList).then(data => {
+                console.log(data);
+            }).catch(err => {
+                console.warn(err);
+            });
         }
     }
 };
