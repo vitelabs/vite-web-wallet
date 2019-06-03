16f91380eccfbe8db387a113ea3e068a8277e904
hurrytospring <ddyz1993@163.com>
2019-06-03 19:25:05 +0800

    format

--- src/pages/assets/dialog/tokenInfo/tb.vue
+++ src/pages/assets/dialog/tokenInfo/tb.vue
@@ -80,11 +80,11 @@
     </div>
 </template>
 <script>
-import Pagination from "components/pagination.vue";
-import { getDepositRecords, getWithdrawRecords } from "services/gate.js";
-import shortify from "utils/ellipsisAddr";
-import b from "utils/bigNumber";
-import d from "dayjs";
+import Pagination from 'components/pagination.vue';
+import { getDepositRecords, getWithdrawRecords } from 'services/gate.js';
+import shortify from 'utils/ellipsisAddr';
+import b from 'utils/bigNumber';
+import d from 'dayjs';
 const pageSize = 10;
 
 export default {
@@ -92,7 +92,7 @@ export default {
     props: {
         type: {
             type: String,
-            default: ""
+            default: ''
         },
         token: {
             type: Object,
@@ -105,8 +105,8 @@ export default {
         return {
             currentPage: 1,
             totalPage: 0,
-            inTxExplorerFormat: "",
-            outTxExplorerFormat: "",
+            inTxExplorerFormat: '',
+            outTxExplorerFormat: '',
             tbData: []
         };
     },
@@ -116,7 +116,7 @@ export default {
     },
     filters: {
         dateShow(value) {
-            return d(Number(value)).format("YYYY.MM.DD HH:mm");
+            return d(Number(value)).format('YYYY.MM.DD HH:mm');
         },
         toBasic: (value, decimals) => b.toBasic(value, decimals),
         hashShortify(value) {
@@ -133,27 +133,25 @@ export default {
             if (!hash) {
                 return;
             }
-            window.open(this.inTxExplorerFormat.replace("{$tx}", hash));
+            window.open(this.inTxExplorerFormat.replace('{$tx}', hash));
         },
         gotoOutHash(hash) {
             if (!hash) {
                 return;
             }
-            window.open(this.outTxExplorerFormat.replace("{$tx}", hash));
+            window.open(this.outTxExplorerFormat.replace('{$tx}', hash));
         },
         updateData(pageNum = this.currentPage) {
             if (!this.type) {
                 return;
-            } else if (this.type === "deposit") {
-                getDepositRecords(
-                    {
-                        tokenId: this.token.tokenId,
-                        walletAddress: this.defaultAddr,
-                        pageNum,
-                        pageSize
-                    },
-                    this.token.gateInfo.url
-                )
+            } else if (this.type === 'deposit') {
+                getDepositRecords({
+                    tokenId: this.token.tokenId,
+                    walletAddress: this.defaultAddr,
+                    pageNum,
+                    pageSize
+                },
+                this.token.gateInfo.url)
                     .then(data => {
                         this.totalPage = Math.ceil(data.totalCount / pageSize);
                         this.inTxExplorerFormat = data.inTxExplorerFormat;
@@ -162,16 +160,14 @@ export default {
                         this.currentPage = pageNum;
                     })
                     .catch(e => console.error(e));
-            } else if (this.type === "withdraw") {
-                getWithdrawRecords(
-                    {
-                        tokenId: this.token.tokenId,
-                        walletAddress: this.defaultAddr,
-                        pageNum,
-                        pageSize
-                    },
-                    this.token.gateInfo.url
-                )
+            } else if (this.type === 'withdraw') {
+                getWithdrawRecords({
+                    tokenId: this.token.tokenId,
+                    walletAddress: this.defaultAddr,
+                    pageNum,
+                    pageSize
+                },
+                this.token.gateInfo.url)
                     .then(data => {
                         this.totalPage = Math.ceil(data.totalCount / pageSize);
                         this.inTxExplorerFormat = data.inTxExplorerFormat;
