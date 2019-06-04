f00272c383758a48c74595a7b518592544396a7d
hurrytospring <ddyz1993@163.com>
2019-06-04 17:23:58 +0800

    fix

--- src/components/pie.vue
+++ src/components/pie.vue
@@ -1,73 +1,76 @@
 <template>
     <div class="pie">
-        <svg class="pie__graph">
-            <slot name="colorMap">
-                <defs>
-                    <linearGradient
-                        id="pie_linear0"
-                        x1="0%"
-                        y1="0%"
-                        x2="100%"
-                        y2="100%"
-                    >
-                        <stop offset="0%" stop-color="#19B4FD" />
-                        <stop offset="100%" stop-color="#007AFF" />
-                    </linearGradient>
-                    <linearGradient
-                        id="pie_linear1"
-                        x1="0%"
-                        y1="0%"
-                        x2="100%"
-                        y2="100%"
-                    >
-                        <stop offset="0%" stop-color="#66D6CA" />
-                        <stop offset="100%" stop-color="#0ACDAF" />
-                    </linearGradient>
-                    <linearGradient
-                        id="pie_linear2"
-                        x1="0%"
-                        y1="0%"
-                        x2="100%"
-                        y2="100%"
-                    >
-                        <stop offset="0%" stop-color="#FFD357" />
-                        <stop offset="100%" stop-color="#FFC41D" />
-                    </linearGradient>
-                    <linearGradient
-                        id="pie_linear3"
-                        x1="0%"
-                        y1="0%"
-                        x2="100%"
-                        y2="100%"
-                    >
-                        <stop offset="0%" stop-color="#FF6848" />
-                        <stop offset="100%" stop-color="#FF8633" />
-                    </linearGradient>
-                    <linearGradient
-                        id="pie_linear4"
-                        x1="0%"
-                        y1="0%"
-                        x2="100%"
-                        y2="100%"
-                    >
-                        <stop offset="0%" stop-color="#1F5086" />
-                        <stop offset="100%" stop-color="#004086" />
-                    </linearGradient>
-                </defs>
-            </slot>
-            <g class="arc__group">
-                <path
-                    v-for="(path, i) in paths"
-                    :d="path"
-                    :key="i"
-                    class="arc"
-                    :fill="colorGen(per[i], i)"
-                ></path>
-            </g>
-            <g class="text__group">
-                <text>{{ title }}</text>
-            </g>
-        </svg>
+        <div class="graph-container">
+            <svg class="pie__graph">
+                <slot name="colorMap">
+                    <defs>
+                        <linearGradient
+                            id="pie_linear0"
+                            x1="0%"
+                            y1="0%"
+                            x2="100%"
+                            y2="100%"
+                        >
+                            <stop offset="0%" stop-color="#19B4FD" />
+                            <stop offset="100%" stop-color="#007AFF" />
+                        </linearGradient>
+                        <linearGradient
+                            id="pie_linear1"
+                            x1="0%"
+                            y1="0%"
+                            x2="100%"
+                            y2="100%"
+                        >
+                            <stop offset="0%" stop-color="#66D6CA" />
+                            <stop offset="100%" stop-color="#0ACDAF" />
+                        </linearGradient>
+                        <linearGradient
+                            id="pie_linear2"
+                            x1="0%"
+                            y1="0%"
+                            x2="100%"
+                            y2="100%"
+                        >
+                            <stop offset="0%" stop-color="#FFD357" />
+                            <stop offset="100%" stop-color="#FFC41D" />
+                        </linearGradient>
+                        <linearGradient
+                            id="pie_linear3"
+                            x1="0%"
+                            y1="0%"
+                            x2="100%"
+                            y2="100%"
+                        >
+                            <stop offset="0%" stop-color="#FF6848" />
+                            <stop offset="100%" stop-color="#FF8633" />
+                        </linearGradient>
+                        <linearGradient
+                            id="pie_linear4"
+                            x1="0%"
+                            y1="0%"
+                            x2="100%"
+                            y2="100%"
+                        >
+                            <stop offset="0%" stop-color="#1F5086" />
+                            <stop offset="100%" stop-color="#004086" />
+                        </linearGradient>
+                    </defs>
+                </slot>
+                <g class="arc__group">
+                    <path
+                        v-for="(path, i) in paths"
+                        :d="path"
+                        :key="i"
+                        class="arc"
+                        :fill="colorGen(per[i], i)"
+                    ></path>
+                </g>
+            </svg>
+            <div class="text__group">
+                <span>{{ title }}</span>
+            </div>
+        </div>
+
         <div class="legend__group">
             <div class="legend" v-for="(v, i) in per" :key="i">
                 <svg class="tag">
@@ -79,15 +82,15 @@
     </div>
 </template>
 <script>
-import { path } from 'd3-path';
+import { path } from "d3-path";
 
 const sR = 33;
 const lR = 47;
 
 function genPiePath(data, sR, lR) {
     const startAngle = 0;
-    const polar2cartesian = function (angle, r) {
-        return [ r * Math.cos(angle), r * Math.sin(angle) ];
+    const polar2cartesian = function(angle, r) {
+        return [r * Math.cos(angle), r * Math.sin(angle)];
     };
     let arcStartAngle = startAngle;
     const paths = data.map(v => {
@@ -107,11 +110,11 @@ export default {
     props: {
         colorGen: {
             type: Function,
-            default: (v, i) => `url(#pie_linear${ i })`
+            default: (v, i) => `url(#pie_linear${i})`
         },
         labelGen: {
             type: Function,
-            default: v => `${ (100 * v).toFixed(1) }%`
+            default: v => `${(100 * v).toFixed(1)}%`
         },
         pieData: {
             type: Array,
@@ -119,7 +122,7 @@ export default {
         },
         title: {
             type: String,
-            default: ''
+            default: ""
         }
     },
     computed: {
@@ -145,23 +148,29 @@ export default {
 .pie {
     display: flex;
     align-items: center;
+    .graph-container{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    }
     &__graph {
         height: 94px;
         width: 94px;
-        margin-right: 30px;
     }
-    .arc__group,
-    .text__group {
+    .arc__group {
         transform: translate(50%, 50%);
         .arc {
             transition: ease-in-out 0.1s;
         }
     }
     .text__group {
-        text-anchor: middle;
-        dominant-baseline: middle;
-        fill: #5e6875;
+        max-width: 65px;
+        color: #5e6875;
         font-size: 12px;
+        position: absolute;
+        span {
+            word-break: break-all;
+        }
     }
     .legend__group {
         flex-wrap: wrap;
