281d71272d16f4814d82fe42adca19a4b6ac4f20
hurrytospring <ddyz1993@163.com>
2019-06-27 20:18:47 +0800

    tmp

--- src/wallet/vb.js
+++ src/wallet/vb.js
@@ -1,30 +1,38 @@
+// import Connector from '/Users/yuanzhang/vitecodes/walletconnect-monorepo/packages/browser';
 
+// export const BRIDGE = 'ws://hurrytospring.com:5001';
+// export class VB extends Connector {
+//     constructor(opts) {
+//         super(opts);
+//         this.on('connect', (error, payload) => {
+//             if (error) {
+//                 throw error;
+//             }
+//             this.address = 'vite_ab24ef68b84e642c0ddca06beec81c9acb1977bbd7da27a87a';
+//             // const { accounts, chainId } = payload.params[0];
+//         });
+//         this.on('session_update', (error, payload) => {
+//             if (error) {
+//                 throw error;
+//             }
 
-// export const MODE = {
-//     VITEJS: 'VITEJS',
-//     'VITEBIRFORST': 'VITEBIRFORSAT'
-// };
-// import Connector from 'walletConnector';
-// const bridge = 'https://hurrytospring:5001';
-// class ViteBirforsat {
-//     constructor(birdge, extendAbleNode) {
-//         this.bridge = bridge;
-//         this.extendAbleNode = extendAbleNode;
+//             // Get updated accounts and chainId
+//             this.address = payload.params[0].address;
+//             this.info('session_update');
+//         });
+
+//         this.on('disconnect', (error, payload) => {
+//             if (error) {
+//                 throw error;
+//             }
+
+//             // Delete walletConnector
+//         });
 //     }
 
 //     async createSession() {
-
+//         await super.createSession();
+//         console.log(this.walletConnector.uri);
+//         return this.uri;
 //     }
 // }
-
-export const VB = {
-    createSession() {
-        return 'mock an uri iiiiiii';
-    }
-    // on(event, cb) {
-
-    // },
-    // emit(event) {
-
-    // }
-};
