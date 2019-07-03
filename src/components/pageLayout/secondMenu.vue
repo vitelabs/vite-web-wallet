6e4d9ae5a9843f8514ab7a49d6784f8023a07eae
lovelycs <419579547@qq.com>
2019-07-03 15:37:41 +0800

    go to trade center

--- src/components/pageLayout/secondMenu.vue
+++ src/components/pageLayout/secondMenu.vue
@@ -13,7 +13,7 @@
                 {{ isHaveUsers ? $t('unlockAcc') : $t('login')  }}</div>
             <div v-show="!isLogin" @click="dexChange" class="tab __pointer">
                 {{ isHaveUsers ? $t('changeAcc') : $t('register') }}</div>
-            <div v-show="$route.name.indexOf('trade') !== -1" class="tab __pointer"
+            <div v-show="isHaveUsers && $route.name.indexOf('trade') !== -1" class="tab __pointer"
                  v-unlock-account="showToken" @noactiveacc="dexStart">
                 {{ $t('dexToken') }}</div>
             <switch-addr class="switch-tab menu" v-show="$route.name !== 'assets'" ></switch-addr>
