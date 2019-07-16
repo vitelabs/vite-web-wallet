b11302142e3cdab99edd3452fcda42d2bef712a1
lovelycs <419579547@qq.com>
2019-07-16 19:47:05 +0800

    some

--- src/pages/trade/txPairManage/incomeList.vue
+++ src/pages/trade/txPairManage/incomeList.vue
@@ -2,8 +2,8 @@
     <confirm class="big" :closeIcon="true"
              :title="$t('tradeTxPairManage.incomeList')">
         <div class="total-income">
-            <div class="title"></div>
-            <div class="income"></div>
+            <div class="total-income-title"></div>
+            <div class="income">{{ totalIncome }}</div>
         </div>
         <wallet-table>
             <pagination slot="tableBottom" class="__tb_pagination"
@@ -40,7 +40,9 @@ export default {
         return {
             isLoading: false,
             currentPage: 0,
-            totalNum: 0
+            totalNum: 0,
+            totalIncome: '',
+            list: []
         };
     },
     computed: {
@@ -49,17 +51,64 @@ export default {
         },
         totalPage() {
             return Math.ceil(this.totalNum / 30);
+        },
+        headList() {
+            return [ {
+                text: this.$t('tradeDividend.date'),
+                cell: 'date'
+            }, {
+                text: this.$t('tradeDividend.VX'),
+                cell: 'vxQuantity'
+            }, {
+                text: `BTC ${ this.$t('tradeDividend.amount') }`,
+                cell: 'BTC'
+            }, {
+                text: `ETH ${ this.$t('tradeDividend.amount') }`,
+                cell: 'ETH'
+            }, {
+                text: `USD ${ this.$t('tradeDividend.amount') }`,
+                cell: 'USD'
+            }, {
+                text: `VITE ${ this.$t('tradeDividend.amount') }`,
+                cell: 'VITE'
+            }, {
+                text: this.$t('tradeDividend.price'),
+                cell: 'price'
+            } ];
         }
+        // contentList() {
+        //     const list = [];
+
+        //     this.list.forEach(item => {
+        //         const dividendStat = item.dividendStat || {};
+
+        //         list.push({
+        //             date: date(item.date * 1000, this.$i18n.locale),
+        //             vxQuantity: bigNumber.formatNum(item.vxQuantity, 4),
+        //             ETH: dividendStat.ETH ? this.formatNum(dividendStat.ETH.dividendAmount || 0, 'ETH') : 0,
+        //             VITE: dividendStat.VITE ? this.formatNum(dividendStat.VITE.dividendAmount || 0, 'VITE') : 0,
+        //             BTC: dividendStat.BTC ? this.formatNum(dividendStat.BTC.dividendAmount || 0, 'BTC') : 0,
+        //             USD: dividendStat.USD ? this.formatNum(dividendStat.USD.dividendAmount || 0, 'USD') : 0,
+        //             price: this.getPrice(dividendStat)
+        //         });
+        //     });
+        //     return list;
+        // }
     },
     methods: {
-        fetchIncomeList() {
+        fetchIncomeList(pageNum) {
+            const offset = pageNum ? (pageNum - 1) * 30 : 0;
+
             operatorIncome({
                 operatorId: this.activeAddr,
                 tradeToken: this.txPair.txPairDetail.tradeToken,
-                quoteToken: this.txPair.txPairDetail.quoteToken
-                // offset, limit = 30
+                quoteToken: this.txPair.txPairDetail.quoteToken,
+                offset,
+                limit: 30
             }).then(data => {
                 this.totalNum = data.total || 0;
+                this.totalIncome = data.income || 0;
+                this.list = data ? data.incomeList || [] : [];
             }).catch(err => {
                 console.warn(err);
             });
@@ -76,7 +125,7 @@ export default {
     box-sizing: border-box;
     background: rgba(0,122,255,0.05);
     line-height: 16px;
-    .title {
+    .total-income-title {
         font-size: 12px;
         font-family: font-family-normal();
         color: rgba(94,104,117,1);
