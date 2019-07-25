70118fefad1fd488ec63c7b80ac8197393073336
lovelycs <419579547@qq.com>
2019-07-25 18:16:16 +0800

    usd

--- src/store/exchange/market.js
+++ src/store/exchange/market.js
@@ -1,7 +1,7 @@
 import { baseToken, marketsClosed } from 'services/trade';
 import getQuery from 'utils/query';
 
-const quoteTokenCategory = [ 'VITE', 'BTC', 'ETH', 'USDT' ];
+const quoteTokenCategory = [ 'VITE', 'BTC', 'ETH', 'USD' ];
 
 const query = getQuery();
 const category = query.category;
