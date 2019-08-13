853417e65e5804de3f0ad4c034550012747344b4
lovelycs <419579547@qq.com>
2019-08-13 17:14:46 +0800

    after build

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -24,6 +24,7 @@ const locale = localStorage.getItem('lang') || appLocale || getDefaultLang() || 
 const i18nConf = {
     locale,
     fallbackLocale: 'en',
+    silentFallbackWarn: true,
     messages: { en, zh, id_ID: id }
 };
 
