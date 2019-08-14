03aa336709801d354ee8e9dfc5a566ae063f265c
lovelycs <419579547@qq.com>
2019-08-14 16:46:06 +0800

    some

--- pack/webpack/dev.config.js
+++ pack/webpack/dev.config.js
@@ -2,30 +2,12 @@ module.exports = {
     mode: 'development',
     devtool: 'source-map',
     devServer: {
-        quiet: false,
-        host: '0.0.0.0',
-        port: 8081,
         proxy: {
             '/gw': {
                 target: 'http://132.232.60.116:8000',
                 changeOrigin: true,
                 secure: false
-            },
-            '/trade': {
-                target: 'http://localhost:8081',
-                pathRewrite: { '^/trade\w*': '' }
-            },
-            '/gateway': {
-                target: 'https://gateway.vitewallet.com/test/crosschain/',
-                changeOrigin: true,
-                secure: false,
-                pathRewrite: { '^/gateway': '' }
             }
-            // '/api/v1': {
-            //     target: 'http://192.168.31.189:8082/dev/',
-            //     changeOrigin: true,
-            //     secure: false
-            // }
         }
     }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 const { entry, htmlWebpackPlugins } = require('./pack/webpack/getEntry');
 const baseConfig = require('./pack/webpack/base.config.js');
 const devConfig = require('./pack/webpack/dev.config.js');
-const testConfig = require('./pack/webpack/test.config.js');
 const prodConfig = require('./pack/webpack/prod.config.js');
 
 console.log(`\n ======== process.env.NODE_ENV: ${ process.env.NODE_ENV } ======== \n`);
@@ -20,9 +19,6 @@ let webpackConfig = merge({
 
 if (process.env.NODE_ENV === 'dev') {
     webpackConfig = merge(webpackConfig, devConfig);
-}
-if (process.env.NODE_ENV === 'test') {
-    webpackConfig = merge(webpackConfig, testConfig);
 }
 if (process.env.NODE_ENV === 'production') {
     webpackConfig = merge(webpackConfig, prodConfig);
