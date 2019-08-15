11becb3ef84229e07e5af75ef1cd3cf2ca9fc6b7
lovelycs <419579547@qq.com>
2019-08-15 14:45:06 +0800

    add mobile notfound

---
+++ srcH5/pages/notFound.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="not-found">Not Found</div>
+</template>
+
+<script>
+export default { };
+</script>
+
+<style lang="scss" scoped>
+.not-found {
+    width: 100%;
+    text-align: center;
+    font-size: 40px;
+    margin-top: 200px;
+}
+</style>
