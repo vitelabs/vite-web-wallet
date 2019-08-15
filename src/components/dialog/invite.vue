b7f73f83e8f88c71b16cf6bd4876bc8f95a5c430
hurrytospring <ddyz1993@163.com>
2019-08-15 16:38:08 +0800

    fix

--- src/components/dialog/invite.vue
+++ src/components/dialog/invite.vue
@@ -100,7 +100,7 @@ export default {
             return Promise.reject('no close');
         },
         copyShare() {
-            copy(`https://x.vite.net/trade?inviteCode=${ this.inviteCode }`);
+            copy(`${location.origin}/trade?inviteCode=${ this.inviteCode }`);
             this.$toast(this.$t('hint.copy'));
         },
         copy() {
