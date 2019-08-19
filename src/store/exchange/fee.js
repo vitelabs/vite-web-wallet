23ea31774899142f40572d5ede1ddf0ed7951161
lovelycs <419579547@qq.com>
2019-08-19 17:47:22 +0800

    merge test

--- src/store/exchange/fee.js
+++ src/store/exchange/fee.js
@@ -40,7 +40,7 @@ const actions = {
                 commit('setInviteCode', code);
                 res(code);
             }).catch(e => {
-                commit('setInviteCode');
+                commit('setInviteCode', '-1');
                 rej(e);
             });
         });
