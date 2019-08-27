cd0d07e0546e703eae9a06d6e47e9ac2392eee4a
lovelycs <419579547@qq.com>
2019-08-27 20:14:00 +0800

    pri

--- srcH5/utils/bridge.js
+++ srcH5/utils/bridge.js
@@ -1,7 +1,7 @@
 import Bridge from '@vite/bridge';
 import inviteRR from 'h5Assets/imgs/inviteRR.png';
 
-export const bridge = new Bridge({ selfDefinedMethods: [ 'wallet.sendRawTx', 'nav.RRBtnClick' ] });
+export const bridge = new Bridge({ selfDefinedMethods: [ 'pri.sendTx', 'nav.RRBtnClick' ] });
 
 bridge['app.setWebTitle']({ title: 'ViteX' }).then(() => {
     console.log('setWebTitle ok');
