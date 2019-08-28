dc6655f1263c625df39646b2cb591d03e4e726a3
lovelycs <419579547@qq.com>
2019-08-28 17:59:12 +0800

    bottombar

--- srcH5/pages/index.vue
+++ srcH5/pages/index.vue
@@ -3,6 +3,7 @@
         <div class="router-wrapper">
             <router-view></router-view>
         </div>
+        <bottom-bar class="bottom-bar"></bottom-bar>
         <order-notice-list class="notice-list"></order-notice-list>
     </div>
 </template>
@@ -13,9 +14,10 @@ import orderNoticeList from 'components/orderNoticeList.vue';
 import env from 'h5Utils/envFromURL';
 import { setItem } from 'h5Utils/storage';
 import { bridge } from 'h5Utils/bridge';
+import bottomBar from 'h5Components/bottomBar';
 
 export default {
-    components: { orderNoticeList },
+    components: { orderNoticeList, bottomBar },
     mounted() {
         bridge['nav.RRBtnClick'](() => {
             console.log('isDialog??');
@@ -39,9 +41,21 @@ export default {
     width: 100%;
     overflow: hidden;
     .router-wrapper {
-        width: 100%;
-        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 49px;
         overflow: auto;
+    }
+    .bottom-bar {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        height: 49px;
+        box-sizing: border-box;
+        background: rgba(255,255,255,1);
+        box-shadow: 0px -2px 20px 0px rgba(0,0,0,0.05);
     }
 }
 
