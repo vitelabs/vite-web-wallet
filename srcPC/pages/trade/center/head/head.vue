31d875ad6d83230e289fd8ee9ef48a7e912596a8
lovelycs <419579547@qq.com>
2019-09-03 11:13:14 +0800

    3 amount

--- srcPC/pages/trade/center/head/head.vue
+++ srcPC/pages/trade/center/head/head.vue
@@ -40,7 +40,7 @@
                 <div class="quantity item-left">
                     <div class="token-title">{{ $t('trade.head.quantity') }}</div>
                     <div class="token-content">
-                        {{ activeTxPair && activeTxPair.amount ? formatNum(activeTxPair.amount, 1) + ' ' + activeTxPair.originQuoteTokenSymbol : '--' }}
+                        {{ activeTxPair && activeTxPair.amount ? formatNum(activeTxPair.amount, 3) + ' ' + activeTxPair.originQuoteTokenSymbol : '--' }}
                     </div>
                 </div>
             </div>
