f06d94f040465327e2450804088a51441179aac7
lovelycs <419579547@qq.com>
2019-09-11 14:26:34 +0800

    usd to usdt

--- srcH5/pages/mining/dividend/pool.vue
+++ srcH5/pages/mining/dividend/pool.vue
@@ -38,7 +38,7 @@ const typeList = [ {
     name: 'ETH',
     icon: ethIcon
 }, {
-    name: 'USD',
+    name: 'USDT',
     icon: usdIcon
 } ];
 
@@ -63,7 +63,7 @@ export default {
                 return `≈${ pre } --`;
             }
 
-            allPrice = this.formatNum(allPrice, 'USD');
+            allPrice = this.formatNum(allPrice, 'USDT');
             return `≈${ pre }${ allPrice }`;
         },
         allBtc() {
@@ -115,7 +115,7 @@ export default {
 
                 this.pool = {};
                 const tokenIds = [];
-                const tokenList = [ 'VITE', 'ETH', 'BTC', 'USD' ];
+                const tokenList = [ 'VITE', 'ETH', 'BTC', 'USDT' ];
 
                 for (const tokenId in data) {
                     const token = data[tokenId];
@@ -148,7 +148,7 @@ export default {
                 BTC: 8,
                 ETH: 8,
                 VITE: 4,
-                USD: 2
+                USDT: 2
             };
             return bigNumber.formatNum(amount, map[tokenSymbol]);
         }
