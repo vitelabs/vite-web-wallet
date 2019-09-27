9eea679a960c23baca5a3851ebced079622fbd26
lovelycs <419579547@qq.com>
2019-09-27 19:17:30 +0800

    build

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,6 +2,18 @@ echo "Start deploy";
 
 handle=$1;
 
+# upload
+# if [ $handle == "PC" ]
+# then
+#     echo "Upload PC =================";
+#     scp ./static.tar vite_wallet_nginx:/home/ubuntu/vite-web-wallet/static.tar
+# else
+#     echo "Upload H5 =================";
+#     scp ./h5Static.tar vite_wallet_nginx:/home/ubuntu/vite-web-wallet/h5Static.tar
+# fi
+
+# echo "Upload FINISH=================";
+
 if [ $handle == "PC" ]
 then
     # ssh vite_wallet_nginx "cd ~/vite-web-wallet && rm -rf ./static ./dist && tar -xvf static.tar && mv ./dist ./static"
