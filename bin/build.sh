a7ec06ad11e552ec85f2cb4c9dd517cd73013827
lovelycs <419579547@qq.com>
2019-09-27 18:04:10 +0800

    some

--- bin/build.sh
+++ bin/build.sh
@@ -4,24 +4,24 @@ handle=$1;
 isClear=$2;
 
 # Pull and clear and install
-git checkout master
+# git checkout master
 
-echo "Pull ==============";
-git pull
+# echo "Pull ==============";
+# git pull
 
-if [ "$isClear" == "clear" ]
-then
-    echo "Delete node_modules =================";
-    rm -rf ./node_modules
-fi
+# if [ "$isClear" == "clear" ]
+# then
+#     echo "Delete node_modules =================";
+#     rm -rf ./node_modules
+# fi
 
-echo "Install ===============";
-yarn
+# echo "Install ===============";
+# yarn
 
 # Build and tar
 echo "Build" $handle;
 
-if [ $handle == "PC" ]
+if [ "$handle"X == "PC"X ]
 then
     echo "Delete dist =================";
     rm -rf ./dist
@@ -35,7 +35,19 @@ else
     echo "Build =================";
     yarn run build:H5
     echo "Tar ==============";
-    tar -cvf h5Static.tar.tar ./distH5/*
+    tar -cvf h5Static.tar ./distH5/*
 fi
+
+# upload
+# if [ $handle == "PC" ]
+# then
+#     echo "Upload PC =================";
+#     scp ./static.tar vite_wallet_nginx:/home/ubuntu/vite-web-wallet/static.tar
+# else
+#     echo "Upload H5 =================";
+#     scp ./h5Static.tar vite_wallet_nginx:/home/ubuntu/vite-web-wallet/h5Static.tar
+# fi
+
+# echo "Upload FINISH=================";
 
 echo "Finish building";
