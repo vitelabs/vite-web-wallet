51d76caca48268f655bc515d904e881e4110f1f3
lovelycs <419579547@qq.com>
2019-09-29 13:56:19 +0800

    deploy change

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,14 +1,14 @@
-echo "Start deploy";
-
 handle=$1;
 
-if [ $handle == "PC" ]
+echo "Start deploy";
+
+if [ "$handle"X == "PC"X ]
 then
-    # ssh vite_wallet_nginx "cd ~/vite-web-wallet && rm -rf ./static ./dist && tar -xvf static.tar && mv ./dist ./static"
     echo "Deploy PC"
+    ssh vite_wallet_deploy "cd ~/vite-web-wallet && rm -rf ./static ./dist && tar -xvf static.tar && mv ./dist ./static"
 else
-    # ssh vite_wallet_nginx "cd ~/vite-web-wallet && rm -rf ./distH5 ./mobiledex && tar -xvf h5Static.tar && mv ./distH5 ./mobiledex"
     echo "Deploy H5"
+    ssh vite_wallet_deploy "cd ~/vite-web-wallet && rm -rf ./distH5 ./mobiledex && tar -xvf h5Static.tar && mv ./distH5 ./mobiledex"
 fi
 
 echo "Finish deploy";
