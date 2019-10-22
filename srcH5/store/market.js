8c9f988909fb4cc95b1b49457e1267334b7c2df6
lovelycs <419579547@qq.com>
2019-10-22 15:31:22 +0800

    switch txpair

--- srcH5/store/market.js
+++ srcH5/store/market.js
@@ -2,7 +2,6 @@ import { marketsClosed } from 'services/trade';
 import env from 'h5Utils/envFromURL';
 
 const state = {
-    // DefaultSymbol: symbol,
     currentSymbol: env.symbol,
     curentCategory: env.category,
     marketClosed: []
@@ -11,18 +10,10 @@ const state = {
 const mutations = {
     setMarketClosed(state, marketClosed) {
         state.marketClosed = marketClosed;
+    },
+    switchTradePair(state, { symbol }) {
+        state.currentSymbol = symbol;
     }
-    // setCurentCategory(state, category) {
-    //     state.curentCategory = category;
-    // },
-    // switchTradePair(state, { category, symbol }) {
-    //     if (!category || quoteTokenCategory.indexOf(category) === -1) {
-    //         return;
-    //     }
-
-    //     state.curentCategory = category;
-    //     state.DefaultSymbol = symbol;
-    // }
 };
 
 const actions = {
