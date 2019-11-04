b17582a7ca0d1aa452cdeab56424791d791aec71
lovelycs <419579547@qq.com>
2019-11-04 22:20:58 +0800

    some

--- pack/webpack/prod.config.js
+++ pack/webpack/prod.config.js
@@ -2,20 +2,6 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     mode: 'production',
-    devServer: {
-        proxy: {
-            '/gw': {
-                target: 'https://x.vite.net',
-                changeOrigin: true,
-                secure: false
-            },
-            '/dns/*': {
-                target: 'https://config.vitewallet.com/',
-                changeOrigin: true,
-                secure: false
-            }
-        }
-    },
     optimization: {
         minimizer: [
             new UglifyJsPlugin({
