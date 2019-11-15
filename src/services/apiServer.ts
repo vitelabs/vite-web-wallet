0e7c8c02602710c9ab9211369c987e5b25596451
lovelycs <419579547@qq.com>
2019-11-15 17:51:17 +0800

    add quota

--- src/services/apiServer.ts
+++ src/services/apiServer.ts
@@ -39,3 +39,32 @@ const WS_RPC = new provider(url);
 export const viteClient = new client(WS_RPC, () => {
     console.log('Connect success');
 });
+
+const superVipAbi = { 'type': 'function', 'name': 'StakeForSVIP', 'inputs': [{ 'name': 'actionType', 'type': 'uint8' }] };
+const StakeForPrincipalSVIPAbi = { 'type': 'function', 'name': 'StakeForPrincipalSVIP', 'inputs': [ { 'name': 'actionType', 'type': 'uint8' }, { 'name': 'principal', 'type': 'address' } ] };
+const StakeForVIPAbi = { 'type': 'function', 'name': 'StakeForVIP', 'inputs': [{ 'name': 'actionType', 'type': 'uint8' }] };
+const autoLockAbi = { 'type': 'function', 'name': 'SwitchConfig', 'inputs': [ { 'name': 'switchType', 'type': 'uint8' }, { 'name': 'enable', 'type': 'bool' } ] };
+const lockVXAbi = { 'type': 'function', 'name': 'LockVxForDividend', 'inputs': [ { 'name': 'actionType', 'type': 'uint8' }, { 'name': 'amount', 'type': 'uint256' } ] };
+
+viteClient.addTxType({
+    SwitchConfig: {
+        contractAddr: 'vite_0000000000000000000000000000000000000006e82b8ba657',
+        abi: autoLockAbi
+    },
+    LockVxForDividend: {
+        contractAddr: 'vite_0000000000000000000000000000000000000006e82b8ba657',
+        abi: lockVXAbi
+    },
+    StakeForVIP: {
+        contractAddr: 'vite_0000000000000000000000000000000000000006e82b8ba657',
+        abi: StakeForVIPAbi
+    },
+    StakeForSVIP: {
+        contractAddr: 'vite_0000000000000000000000000000000000000006e82b8ba657',
+        abi: superVipAbi
+    },
+    StakeForPrincipalSVIP: {
+        contractAddr: 'vite_0000000000000000000000000000000000000006e82b8ba657',
+        abi: StakeForPrincipalSVIPAbi
+    }
+});
