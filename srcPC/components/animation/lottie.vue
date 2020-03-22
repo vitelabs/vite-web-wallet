64708d5689115bf29447d792b7fa886de7ad5573
soliury <soliury@pm.me>
2020-03-22 18:49:48 +0800

    eslint

--- srcPC/components/animation/lottie.vue
+++ srcPC/components/animation/lottie.vue
@@ -4,34 +4,32 @@
 
 
 <script>
-import lottie from "lottie-web";
+import lottie from 'lottie-web';
 
 const getData = {
-    plugAndPinCode: async (theme) => {
+    plugAndPinCode: async theme => {
         if (!theme) {
             return import('./plugAndPinCode/light.json');
         }
         return import('./plugAndPinCode/dark.json');
     },
-    openApp: async (theme) => {
+    openApp: async theme => {
         if (!theme) {
             return import('./openApp/light.json');
         }
         return import('./openApp/dark.json');
     },
-    validate: async (theme) => {
+    validate: async theme => {
         if (!theme) {
             return import('./validate/light.json');
         }
         return import('./validate/dark.json');
     }
-}
+};
 
 export default {
     props: {
-        type: {
-            required: true
-        },
+        type: { required: true },
         speed: {
             type: Number,
             required: false,
@@ -48,20 +46,18 @@ export default {
             default: -1
         },
         loop: {
-            type:Boolean,
+            type: Boolean,
             required: false,
             default: true
         },
         autoPlay: {
-            type:Boolean,
+            type: Boolean,
             required: false,
             default: true
         }
     },
     data: () => {
-        return {
-            data: null
-        }
+        return { data: null };
     },
     computed: {
         theme() {
@@ -72,22 +68,22 @@ export default {
         this.init();
     },
     watch: {
-        type: function(newValue, oldValue) {
+        type: function (newValue, oldValue) {
             this.init();
         },
-        theme: function(newValue, oldValue) {
+        theme: function (newValue, oldValue) {
             this.init();
         }
     },
     methods: {
         async init() {
-            let data = await getData[this.type](this.theme);
+            const data = await getData[this.type](this.theme);
 
             if (this.lottie) this.lottie.destroy();
 
             this.lottie = lottie.loadAnimation({
                 container: this.$refs.lottieContainer,
-                renderer: "svg",
+                renderer: 'svg',
                 loop: this.loop,
                 autoplay: this.autoPlay,
                 animationData: data
