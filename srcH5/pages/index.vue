7182982b2b3a851873b3a1ed213f5116166fd8c4
soliury <lingyong1992@foxmail.com>
2020-04-17 20:28:38 +0800

    bugfix: mining page can not scroll

--- srcH5/pages/index.vue
+++ srcH5/pages/index.vue
@@ -66,14 +66,14 @@ export default {
     top: 0;
     left: 0;
     right: 0;
-    bottom: 0;
+    bottom: 1px;
     overflow: hidden;
     .router-wrapper {
         position: fixed;
         top: 0;
         left: 0;
         right: 0;
-        bottom: 0;
+        bottom: 1px;
         overflow: hidden;
     }
     .bottom-bar {
