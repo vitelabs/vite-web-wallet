9bc995a6488da64a69600882e5598cbe84517da2
soliury <lingyong1992@foxmail.com>
2020-04-21 17:57:36 +0800

    defi: change api

--- srcPC/services/defi.ts
+++ srcPC/services/defi.ts
@@ -1,4 +1,5 @@
 import { ViteXAPI } from 'services/apiServer';
+import Vite from '@vite/ledgerjs-hw-app-vite';
 
 // defi: Get global overview of defi
 export async function getGlobalAssets() {
@@ -7,28 +8,99 @@ export async function getGlobalAssets() {
         path: '/finance/global'
     }).catch(() => [
         {
-            tokendId: 'tti_5649544520544f4b454e6e40',
+            tokenId: 'tti_5649544520544f4b454e6e40',
             symbol: 'VITE',
             lendQty: 200,
-            depositQty: 11111,
+            depositQty: '1234567891234567891',
             depositRatio: '',
             lendRatio: '',
-            lendDepositRatio: '',
-            depositRate: '',
-            lendRate: '',
-            userCount: 100
+            lendDepositRatio: 10,
+            depositRate: 15,
+            lendRate: 11,
+            lendNumber: 100,
+            depositNumber: 11111
         },
         {
-            tokendId: 'tti_06822f8d096ecdf9356b666c',
+            tokenId: 'tti_06822f8d096ecdf9356b666c',
             symbol: 'ETH',
             lendQty: 200,
+            depositQty: '111111111111111',
+            depositRatio: '',
+            lendRatio: '',
+            lendDepositRatio: 10,
+            depositRate: 13,
+            lendRate: 4,
+            lendNumber: 100,
+            depositNumber: 11111
+        }
+    ]);
+}
+
+export async function getAssetMeta() {
+    return ViteXAPI.request({
+        method: 'GET',
+        path: '/coins'
+    }).catch(() => [
+        {
+            logo: '',
+            tokenId: 'tti_5649544520544f4b454e6e40',
+            symbol: 'VITE',
+            name: 'Vite',
+            decimals: 18,
+            usdPrice: 0.1,
+            btcPrice: 0.000001,
+            cnyPrice: 10,
+            stakingDiscount: 11
+        },
+        {
+            logo: '',
+            tokenId: 'tti_06822f8d096ecdf9356b666c',
+            symbol: 'ETH',
+            name: 'Eth',
+            decimals: 18,
+            usdPrice: 170,
+            btcPrice: 0.025,
+            cnyPrice: 1000,
+            stakingDiscount: 11
+        }
+    ]);
+}
+
+export async function getUserAssets(address) {
+    return ViteXAPI.request({
+        method: 'GET',
+        path: '/finance/my',
+        params: { address }
+    }).catch(() => [
+        {
+            tokenId: 'tti_5649544520544f4b454e6e40',
+            symbol: 'VITE',
+            lendQty: 200,
+            maxLendQty: 1000,
             depositQty: 11111,
             depositRatio: '',
             lendRatio: '',
             lendDepositRatio: '',
             depositRate: '',
             lendRate: '',
-            userCount: 1001
+            staking: false,
+            restDepositInterest: 111,
+            restLendInterest: 120
+        },
+        {
+            tokenId: 'tti_06822f8d096ecdf9356b666c',
+            symbol: 'ETH',
+            lendQty: 200,
+            maxLendQty: 1000,
+            depositQty: 11111,
+            depositRatio: '',
+            lendRatio: '',
+            lendDepositRatio: '',
+            depositRate: '',
+            lendRate: '',
+            staking: false,
+            restDepositInterest: 111,
+            restLendInterest: 120
         }
     ]);
 }
