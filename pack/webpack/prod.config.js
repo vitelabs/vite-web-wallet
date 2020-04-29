700dd58c4ea478fbf0b6e5fca33bb57b2f08ae46
soliury <lingyong1992@foxmail.com>
2020-04-29 18:56:15 +0800

    disable UglifyJsPlugin

--- pack/webpack/prod.config.js
+++ pack/webpack/prod.config.js
@@ -4,22 +4,22 @@ module.exports = {
     mode: 'production',
     optimization: {
         minimizer: [
-            new UglifyJsPlugin({
-                cache: true,
-                parallel: true,
-                uglifyOptions: {
-                    compress: {
-                        // collapse_vars: true,
-                        // reduce_vars: true,
-                        unused: true,
-                        drop_console: true,
-                        drop_debugger: true
-                    },
-                    output: { comments: false }
-                },
-                extractComments: true,
-                sourceMap: false
-            })
+            // new UglifyJsPlugin({
+            //     cache: true,
+            //     parallel: true,
+            //     uglifyOptions: {
+            //         compress: {
+            //             // collapse_vars: true,
+            //             // reduce_vars: true,
+            //             unused: true,
+            //             drop_console: true,
+            //             drop_debugger: true
+            //         },
+            //         output: { comments: false }
+            //     },
+            //     extractComments: true,
+            //     sourceMap: false
+            // })
         ]
     }
 };
