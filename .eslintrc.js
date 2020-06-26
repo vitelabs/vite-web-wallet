3a4e8817ac3a47cd410e35675725744eca75c9bc
soliury <soliury@pm.me>
2020-06-27 00:58:32 +0800

    refactor

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,7 +29,7 @@ module.exports = {
         "viteWalletI18n": true,
         "DESKTOP": true,
         "ipcRenderer": true,
-        "promptTouchID": true
+        "touchID": true
     },
     "rules": {
         "eqeqeq": "error",
