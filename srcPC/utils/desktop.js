3403e4e44ab587bf09c49b2757c98ce15bbf02d6
soliury <soliury@pm.me>
2020-06-27 13:00:19 +0800

    refresh data after desktop unlock or resume

---
+++ srcPC/utils/desktop.js
@@ -0,0 +1,31 @@
+function refreshBalance(store) {
+    store.dispatch('startLoopBalance');
+    store.dispatch('startLoopExchangeBalance');
+    store.dispatch('exFetchLatestOrder');
+}
+
+function resetSettings(store) {
+    store.dispatch('onNetStatus');
+    store.dispatch('updateMarketMap');
+    store.dispatch('getMarketsClosed');
+    store.dispatch('getDefaultTokenList');
+    store.dispatch('getAllTokens');
+    store.dispatch('updateGateInfos');
+    store.dispatch('fetchTokenInfoFromGithub');
+    store.dispatch('exFetchLimitAmounts');
+    store.dispatch('fetchUiConfig');
+}
+
+
+export const init = (store) => {
+    if (!window.ipcRenderer) return;
+
+    window.ipcRenderer.on('resume', () => {
+        refreshBalance(store);
+        resetSettings(store);
+    });
+
+    window.ipcRenderer.on('unlock-screen', () => {
+        refreshBalance(store);
+    });
+}
