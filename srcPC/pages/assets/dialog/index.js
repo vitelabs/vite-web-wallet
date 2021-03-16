d07acd7ad28ff42c272d3cd4db2996c03e8876a9
soliury <soliury@pm.me>
2021-03-16 12:15:55 +0800

    refactor some code

--- srcPC/pages/assets/dialog/index.js
+++ srcPC/pages/assets/dialog/index.js
@@ -1,10 +1,10 @@
 import getDialog from 'pcComponents/dialog/utils.js';
-import chargeComp from './charge.vue';
+import depositComp from './deposit.vue';
 import addTokenComp from './addToken.vue';
 import tokenInfoComp from './tokenInfo';
 import withdrawComp from './withdraw.vue';
 
-export const chargeDialog = getDialog(chargeComp);
+export const depositDialog = getDialog(depositComp);
 export const addTokenDialog = getDialog(addTokenComp);
 export const tokenInfoDialog = getDialog(tokenInfoComp);
 export const withdrawDialog = getDialog(withdrawComp);
