20ae56eabe75029d261f8987e031ecd96e5c331b
soliury <soliury@pm.me>
2021-03-16 18:46:15 +0800

    support multi-network withdraw and deposit

--- srcPC/store/gateInfo.js
+++ srcPC/store/gateInfo.js
@@ -1,28 +1,83 @@
 import { getGateInfos } from 'pcServices/gate';
+import { getTokenInfoBatch } from 'services/trade';
 
-const state = { gateInfos: [] };
+
+const state = {
+    gateInfos: [],
+
+    // Identification gate token issued on vite network.
+    idenGateTokenList: []
+};
 
 
 const mutations = {
     setGateInfos(state, payload) {
         state.gateInfos = payload || [];
+    },
+    setIdenGateTokenList(state, payload) {
+        state.idenGateTokenList = payload.map(item => {
+            item.tokenId = item.tokenAddress;
+
+            // // for mock test
+            // if (item.tokenId === 'tti_80f3751485e4e83456059473') {
+            //     item.gatewayInfo.mappedToken = {
+            //         ...item.gatewayInfo.mappedToken,
+            //         url: item.gatewayInfo.url,
+            //         standard: 'ERC20'
+            //     };
+            //     item.gatewayInfo.mappedTokenExtras = [{
+            //         ...item.gatewayInfo.mappedToken,
+            //         url: item.gatewayInfo.url,
+            //         standard: 'TRC20'
+            //     }];
+            // }
+            // // for mock test
+
+            let multiNetwork = [];
+            if (item.gatewayInfo && item.gatewayInfo.mappedTokenExtras) {
+                multiNetwork = [item.gatewayInfo.mappedToken].concat(item.gatewayInfo.mappedTokenExtras);
+            }
+            return {
+                ...item,
+                gateInfo: {
+                    ...item.gatewayInfo,
+                    multiNetwork,
+                    gateway: item.gatewayInfo.name // Compatible with old code
+                }
+            };
+        });
     }
 };
 
 const actions = {
-    updateGateInfos({ commit }) {
-        getGateInfos('/gateWay').then(infos =>
-            commit('setGateInfos', infos));
+    updateGateInfos({ commit, dispatch }) {
+        getGateInfos('/gateWay').then(infos => {
+            commit('setGateInfos', infos);
+            dispatch('getTokenInfoBatch');
+        });
+    },
+    getTokenInfoBatch({ commit, getters }) {
+        const tokenAddresses = Object.keys(getters.mapToken2Gate);
+        getTokenInfoBatch({ tokenAddresses }).then(list => {
+            commit('setIdenGateTokenList', list);
+        });
     }
 };
 
 const getters = {
-    mapToken2Gate(state) {
+    mapToken2Gate(state, getters) {
         const map = {};
+        const { idenGateTokenListMap } = getters;
         state.gateInfos.map(g => g.tokens).reduce(function (pre, cur) {
             return [ ...pre, ...(cur || []) ];
         }, []).forEach(n => {
-            map[n.tokenId] = n;
+            map[n.tokenId] = {
+                ...n,
+                gateInfo: idenGateTokenListMap[n.tokenId] ? idenGateTokenListMap[n.tokenId].gateInfo : {
+                    url: n.url,
+                    gateway: n.gateway
+                }
+            };
         });
         return map;
     },
@@ -30,6 +85,16 @@ const getters = {
         const map = {};
         state.gateInfos.forEach(g => (map[g.name] = g));
         return map;
+    },
+    idenGateTokenListMap(state) {
+        const ob = {};
+        state.idenGateTokenList.forEach(item => {
+            ob[item.tokenAddress] = {
+                ...item,
+                tokenId: item.tokenAddress
+            };
+        });
+        return ob;
     }
 };
 
